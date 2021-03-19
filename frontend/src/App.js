af6bb1a194634d406d1c120948882bd9c59bd218
HeroicHitesh <hiteshkumar_1mv17cs042@sirmvit.edu>
2021-03-19 19:00:37 +0530

    Initial Frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Job Portal</h1>
+      <p>Find your dream job with us!!!</p>
+    </div>
+  );
+}
+
+export default App;
