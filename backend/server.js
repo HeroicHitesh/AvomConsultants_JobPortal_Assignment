b9fbdb69b5173dcecf22d557bd6fa08e68e37c75
HeroicHitesh <hiteshkumar_1mv17cs042@sirmvit.edu>
2021-03-19 23:40:13 +0530

    Create backend entry file - server.js

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passportConfig = require("./utils/passportConfig");
+const cors = require("cors");
+const fs = require("fs");
+
+// MongoDB
+mongoose
+  .connect("mongodb://localhost:27017/jobPortal", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((res) => console.log("Connected to Database"))
+  .catch((err) => console.log(err));
+
+// initialising directories
+if (!fs.existsSync("./public")) {
+  fs.mkdirSync("./public");
+}
+if (!fs.existsSync("./public/resume")) {
+  fs.mkdirSync("./public/resume");
+}
+
+const app = express();
+const port = 3060;
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+// Setting up middlewares
+app.use(cors());
+app.use(express.json());
+app.use(passportConfig.initialize());
+
+// Routing
+app.use("/auth", require("./routes/authRoutes"));
+app.use("/upload", require("./routes/uploadRoutes"));
+
+app.listen(port, () => {
+  console.log(`Server started successfully on port ${port}!`);
+});
