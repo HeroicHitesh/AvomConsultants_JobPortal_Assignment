d89892029c8415b81c52e50dd56627d95f1c3237
HeroicHitesh <hiteshkumar_1mv17cs042@sirmvit.edu>
2021-03-19 20:27:48 +0530

    Create various utility components

---
+++ frontend/src/utils/apiList.js
@@ -0,0 +1,9 @@
+export const server = "http://localhost:4444";
+
+const apiList = {
+  login: `${server}/auth/login`,
+  signup: `${server}/auth/signup`,
+  uploadResume: `${server}/upload/resume`,
+};
+
+export default apiList;
